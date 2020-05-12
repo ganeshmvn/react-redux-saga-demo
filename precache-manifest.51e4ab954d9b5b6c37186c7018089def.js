ce995feed987358150ab148220a44322f91807b2
Murukuntla <ganesh_v_murukuntla@optum.com>
2020-05-12 23:07:37 +0530

    Updates

---
+++ precache-manifest.51e4ab954d9b5b6c37186c7018089def.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dcd6a075fc3cacdd4c19795e4b83fb4",
+    "url": "/react-redux-saga-demo/index.html"
+  },
+  {
+    "revision": "0f6cc0b9b1c29bd8404c",
+    "url": "/react-redux-saga-demo/static/js/2.ee204bc7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-saga-demo/static/js/2.ee204bc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13c0d1bab70af76b9c96",
+    "url": "/react-redux-saga-demo/static/js/main.e68397b9.chunk.js"
+  },
+  {
+    "revision": "386c04bce856609530df",
+    "url": "/react-redux-saga-demo/static/js/runtime-main.550f706a.js"
+  },
+  {
+    "revision": "078aa3f09332c2484dbfd7239ef39e2d",
+    "url": "/react-redux-saga-demo/static/media/loading_spinner.078aa3f0.gif"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-redux-saga-demo/precache-manifest.51e4ab954d9b5b6c37186c7018089def.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-redux-saga-demo/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
